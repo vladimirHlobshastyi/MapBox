172e12e8c1cf4de95402777a6db71b21a2b44607
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-05-18 15:52:20 +0300

    fix GeoJson

---
+++ src/geoJson/geojsonData.js
@@ -0,0 +1,60 @@
+import CherkasyJson from "./Cherkasy.json";
+import ChernihivJson from "./Chernihiv.json";
+import ChernivtsiJson from "./Chernivtsi.json";
+import CrimeaJson from "./Crimea.json";
+import DnipropetrovskJson from "./Dnipropetrovsk.json";
+import KhersonJson from "./Kherson.json";
+import KharkivJson from "./Kharkiv.json";
+import IvanoFrankivskJson from "./Ivano-Frankivsk.json";
+import DonetskJson from "./Donetsk.json";
+import LvivJson from "./Lviv.json";
+import LuhanskJson from "./Luhansk.json";
+import KyivCityJson from "./KyivCity.json";
+import KyivJson from "./Kyiv.json";
+import KirovohradJson from "./Kirovohrad.json";
+import KhmelnytskyiJson from "./Khmelnytskyi.json";
+import RivneJson from "./Rivne.json";
+import PoltavaJson from "./Poltava.json";
+import OdessaJson from "./Odessa.json";
+import MykolaivJson from "./Mykolaiv.json";
+import ZakarpattiaJson from "./Zakarpattia.json";
+import VolynJson from "./Volyn.json";
+import VinnytsiaJson from "./Vinnytsia.json";
+import TernopilJson from "./Ternopil.json";
+import SumyJson from "./Sumy.json";
+import SevastopolJson from "./Sevastopol.json";
+import ZaporizhiaJson from "./Zaporizhia.json";
+import ZhytomyrJson from "./Zhytomyr.json";
+import AllRegionsJson from "./AllRegion.json";
+
+const geojsonData = {
+  AllRegions: AllRegionsJson,
+  Cherkasy: CherkasyJson,
+  Chernihiv: ChernihivJson,
+  Chernivtsi: ChernivtsiJson,
+  Crimea: CrimeaJson,
+  Dnipropetrovsk: DnipropetrovskJson,
+  Donetsk: DonetskJson,
+  IvanoFrankivsk: IvanoFrankivskJson,
+  Kharkiv: KharkivJson,
+  Kherson: KhersonJson,
+  Khmelnytskyi: KhmelnytskyiJson,
+  Kirovohrad: KirovohradJson,
+  Kyiv: KyivJson,
+  KyivCity: KyivCityJson,
+  Luhansk: LuhanskJson,
+  Lviv: LvivJson,
+  Mykolaiv: MykolaivJson,
+  Odessa: OdessaJson,
+  Poltava: PoltavaJson,
+  Rivne: RivneJson,
+  Sevastopol: SevastopolJson,
+  Sumy: SumyJson,
+  Ternopil: TernopilJson,
+  Vinnytsia: VinnytsiaJson,
+  Volyn: VolynJson,
+  Zakarpattia: ZakarpattiaJson,
+  Zaporizhia: ZaporizhiaJson,
+  Zhytomyr: ZhytomyrJson,
+};
+export default geojsonData;
