7f610871dbee9ae8285df60ad7964a06f3817e2a
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-05-24 22:08:52 +0300

    fix some elements

--- src/pages/MapView/MapView.jsx
+++ src/pages/MapView/MapView.jsx
@@ -59,39 +59,7 @@ export const MapView = () => {
   };
 
   const host = "https://alerts.com.ua/api/states/live";
-  /* 
-  const logEvent = useCallback((data, dataNew) => {
-    const result = data.filter((item) => {
-      if (item.id === dataNew.id) {
-        return dataNew;
-      } else {
-        return item;
-      }
-    });
-    setData(result);
-  }, []);
-
-  const realTimeConnect = useCallback(() => {
-    const es = new EventSourcePolyfill(host, {
-      headers: { "X-API-Key": "cc3698331afa6f399b17bcf21a33c1faa6bda749" },
-      credentials: "include",
-    });
-    es.addEventListener("open", (e) => console.log("open"));
-    es.addEventListener("error", (e) => {
-      console.log(`error = > ${e.error}`);
-      es.close();
-    });
-    ["hello", "ping", "update"].map((type) =>
-      es.addEventListener(type, (e) => {
-        if (!e.data && type === "update") {
-          console.log(e.data.state);
-          logEvent(data, e.data.state);
-        } else {
-          console.log(e);
-        }
-      })
-    );
-  }, []); */
+  
 
   useEffect(() => {
     console.log(isClosed);
