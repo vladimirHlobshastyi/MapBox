31120e2fe9c391ad98c21bf2055981d2c8805b57
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-08-06 16:02:34 +0300

    add typescript

--- src/providers/AlertProvider.tsx
+++ src/providers/AlertProvider.tsx
@@ -1,15 +1,29 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, FC } from "react";
 import useInterval from "../hooks/useInterval";
 import getAllRegions from "../api/getAllRegions";
 
 // Minor: Split timer and fetching regions. Move timer to separate context/provider.
-
-export const EventsContext = createContext();
+type dataType = { states: Array<alertType>; last_update: string };
+type alertType = {
+  id: number;
+  name: string;
+  name_en: string;
+  alert: Boolean;
+  changed: string;
+};
+interface contextTypeS {
+  alerts: Array<alertType> | [];
+  lastUpdate: string | undefined;
+  isLoading: boolean;
+  errorMessage: string;
+  timerValue: number;
+}
+export const EventsContext = createContext<contextTypeS | null>(null);
 
 const AlertProvider = ({ children }) => {
   const [timerValue, srtTimerValue] = useState(10);
-  const [alerts, setAlerts] = useState([]);
-  const [lastUpdate, setLastUpdate] = useState(undefined);
+  const [alerts, setAlerts] = useState<[] | Array<alertType>>([]);
+  const [lastUpdate, setLastUpdate] = useState<undefined | string>(undefined);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -20,7 +34,7 @@ const AlertProvider = ({ children }) => {
       }
 
       setIsLoading(true);
-      const data = await getAllRegions();
+      const data: dataType = await getAllRegions();
       console.log(data);
       if (data?.states) {
         setAlerts(data?.states);
