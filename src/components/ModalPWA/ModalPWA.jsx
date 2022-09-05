c346b91b1c7657427c7796a9c5a9a54c175efb27
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-09-05 16:37:19 +0300

    add modalPWA

---
+++ src/components/ModalPWA/ModalPWA.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PWAPrompt from 'react-ios-pwa-prompt';
+
+const ModalPWA = () => {
+  return (
+    <PWAPrompt
+      promptOnVisit={1}
+      timesToShow={3}
+      delay={3000}
+      permanentlyHideOnDismiss={false}
+      copyTitle={'Встановити додаток на смартфон'}
+      copyBody={
+        'Ви маєте можливість встановити сайт "повітрянніх тривог" як додаток на ваш смартфон.'
+      }
+      copyShareButtonLabel={
+        'Натисніть іконку "Поділитися" у строці меню унизу.'
+      }
+      copyAddHomeButtonLabel={'Натисніть "Додати на головний екран".'}
+      copyClosePrompt={'Зачинити'}
+    />
+  );
+};
+
+export default ModalPWA;
