b8ba6af5a76c328b690ef915f2c0e1ef99707274
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-09-15 15:03:43 +0300

    fix type

--- src/providers/AlertProvider.types.ts
+++ src/providers/AlertProvider.types.ts
@@ -2,7 +2,7 @@ import { Alert } from '../commonTypes/alert';
 
 export type AlertProviderContextTypes = {
   alerts: Array<Alert>,
-  lastUpdate: string | undefined,
+  lastUpdate: string ,
   isLoading: boolean,
   errorMessage: string,
   timerValue: number,
