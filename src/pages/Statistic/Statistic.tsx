b55d4423ed7f21453f6541e2868543b75ed7e96f
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-10-13 16:33:11 +0300

    refactored style

--- src/pages/Statistic/Statistic.tsx
+++ src/pages/Statistic/Statistic.tsx
@@ -26,24 +26,34 @@ const Statistic: FC<{}> = (): JSX.Element => {
         <></>
       )}
       <div className={style.ContainerWrapper}>
-        <div className={style.screenshotForStatistic} ref={myRef}>
-          <div className={style.Header}>
-            <div className={style.HeaderEmblem}>
+        <div className={style.ContainerWrapperScreenshot} ref={myRef}>
+          <div className={style.ContainerWrapperScreenshotHeader}>
+            <div className={style.ContainerWrapperScreenshotHeaderEmblem}>
               <a href="https://www.mil.gov.ua">
                 <img src={emblem} alt="ZSU logo" />
               </a>
             </div>
-            <div className={style.HeaderInfo}>
-              <div className={style.HeaderInfoH}>
+            <div className={style.ContainerWrapperScreenshotHeaderInfo}>
+              <div className={style.ContainerWrapperScreenshotHeaderInfoH}>
                 <h2>ГЕНЕРАЛЬНИЙ ШТАБ ЗС УКРАЇНИ</h2>
                 <h1>ІНФОРМУЄ</h1>
               </div>
-              <div className={style.HeaderInfoSpan}>
-                <div className={style.HeaderInfoSpanDate}>
-                  <div className={style.HeaderInfoSpanDateEl}>
+              <div className={style.ContainerWrapperScreenshotHeaderInfoSpan}>
+                <div
+                  className={style.ContainerWrapperScreenshotHeaderInfoSpanDate}
+                >
+                  <div
+                    className={
+                      style.ContainerWrapperScreenshotHeaderInfoSpanDateEl
+                    }
+                  >
                     <span>ПРОТЯГОМ 24.02-</span>
                   </div>
-                  <div className={style.HeaderInfoSpanDateEl}>
+                  <div
+                    className={
+                      style.ContainerWrapperScreenshotHeaderInfoSpanDateEl
+                    }
+                  >
                     <h2>{reverseDate}</h2>
                   </div>
                 </div>
