a99903f54e43083e43b496f26ca390f0da67035f
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-10-19 20:06:26 +0300

    test sw

--- src/components/Legend/Legend.tsx
+++ src/components/Legend/Legend.tsx
@@ -15,14 +15,22 @@ const Legend = () => {
     } else if (Notification.permission === 'granted') {
       // Check whether notification permissions have already been granted;
       // if so, create a notification
-      const notification = new Notification('Hi there!');
+      const notification = new Notification('Hi there!', {
+        data: 'Hi there granted!',
+        vibrate: [50, 10, 50],
+      });
+      debugger;
       return notification;
     } else if (Notification.permission !== 'denied') {
       // We need to ask the user for permission
       Notification.requestPermission().then((permission) => {
         // If the user accepts, let's create a notification
         if (permission === 'granted') {
-          const notification = new Notification('Hi there!');
+          const notification = new Notification('Hi there!', {
+            data: 'Hi there! !denied',
+            vibrate: [50, 10, 50],
+          });
+          debugger;
           return notification;
         }
       });
