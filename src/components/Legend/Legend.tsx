8d9fb45e239d23bb1a606dab185d557e9ef57c49
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-10-20 13:49:26 +0300

    test sw

--- src/components/Legend/Legend.tsx
+++ src/components/Legend/Legend.tsx
@@ -15,20 +15,19 @@ const Legend = () => {
     } else if (Notification.permission === 'granted') {
       // Check whether notification permissions have already been granted;
       // if so, create a notification
-      const notification = new Notification('Vibration Sample', {
-        body: 'Buzz! Buzz!',
-        data: 'ALARM!',
-        badge: './../Info/png/special_military_equip.png',
-        icon: './../Info/png/special_military_equip.png',
-        image: './../Info/png/special_military_equip.png',
-        vibrate: [100, 50, 100, 100, 50],
-        tag: `${new Date()}`,
-      });
-
-      document.addEventListener('visibilitychange', () => {
-        if (document.visibilityState === 'visible') {
-          // The tab has become visible so clear the now-stale Notification.
-          notification.close();
+      const notification = Notification.requestPermission((result) => {
+        if (result === 'granted') {
+          navigator.serviceWorker.ready.then((registration) => {
+            registration.showNotification('Vibration Sample', {
+              body: 'Buzz! Buzz!',
+              data: 'ALARM!',
+              badge: './../Info/png/special_military_equip.png',
+              icon: './../Info/png/special_military_equip.png',
+              image: './../Info/png/special_military_equip.png',
+              vibrate: [100, 50, 100, 100, 50],
+              tag: `${new Date()}`,
+            });
+          });
         }
       });
     } else if (Notification.permission !== 'denied') {
@@ -36,7 +35,7 @@ const Legend = () => {
       Notification.requestPermission().then((permission) => {
         // If the user accepts, let's create a notification
         if (permission === 'granted') {
-          /*  const notification = Notification.requestPermission((result) => {
+          const notification = Notification.requestPermission((result) => {
             if (result === 'granted') {
               navigator.serviceWorker.ready.then((registration) => {
                 registration.showNotification('Vibration Sample', {
@@ -50,9 +49,9 @@ const Legend = () => {
                 });
               });
             }
-          }); */
+          });
 
-          const notification = new Notification('Vibration Sample', {
+          /*   const notification = new Notification('Vibration Sample', {
             body: 'Buzz! Buzz!',
             data: 'ALARM!',
             badge: './../Info/png/special_military_equip.png',
@@ -60,14 +59,7 @@ const Legend = () => {
             image: './../Info/png/special_military_equip.png',
             vibrate: [100, 50, 100, 100, 50],
             tag: `${new Date()}`,
-          });
-
-          document.addEventListener('visibilitychange', () => {
-            if (document.visibilityState === 'visible') {
-              // The tab has become visible so clear the now-stale Notification.
-              notification.close();
-            }
-          });
+          }); */
         }
       });
     }
@@ -75,11 +67,11 @@ const Legend = () => {
     // At last, if the user has denied notifications, and you
     // want to be respectful there is no need to bother them anymore.
   }
-  useEffect(() => {
-    setInterval(() => {
-      notifyMe();
-    }, 4000);
-  }, []);
+
+  setInterval(() => {
+    notifyMe();
+  }, 4000);
+
   return (
     <div className={style.Legend} onClick={/* updateAlerts */ notifyMe}>
       <Tag color={'red'} text={'Тривога в регіоні'} />
