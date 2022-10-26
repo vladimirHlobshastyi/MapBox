94daccaa9b89dbcbca0f264206f09368b11276e1
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-10-26 10:47:46 +0300

    change badge

--- src/providers/NotificationProvider.tsx
+++ src/providers/NotificationProvider.tsx
@@ -69,7 +69,7 @@ const NotificationProvider = ({ children }: ChildrenPropTypes) => {
       (registration) => {
         registration.showNotification(isAlert(), {
           body: alerts ? 'У вашому регіоні оголошена повітряна тривога!' : 'У вашому регіоні відмінена повітряна тривога!',
-          badge: './logo16.ico',
+          badge: './baseline_report_black_18.png',
           icon: './logo192.png',
           vibrate: [200, 100, 300],
           requireInteraction: false,
