142443e8e874b58dc8804f908fd072ff20e3d6c0
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-10-26 11:45:55 +0300

    change vibro

--- src/providers/NotificationProvider.tsx
+++ src/providers/NotificationProvider.tsx
@@ -69,9 +69,9 @@ const NotificationProvider = ({ children }: ChildrenPropTypes) => {
       (registration) => {
         registration.showNotification(isAlert(), {
           body: alerts ? 'У вашому регіоні оголошена повітряна тривога!' : 'У вашому регіоні відмінена повітряна тривога!',
-          badge: './logo512.png',
-          icon: './logo192.png',
-          vibrate: [200, 100, 300],
+          badge: './image/logo192.png',
+          icon: './image/logo192.png',
+          vibrate: [300, 100, 300],
           requireInteraction: false,
           renotify: true,
           tag: 'notification'
